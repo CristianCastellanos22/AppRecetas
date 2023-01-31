92735337014ce65e1964b3f67e97f1de073ea406
ccastellanostribal <ccastellanos@tribalworldwide.gt>
2023-01-30 21:31:07 -0500

    Add string resources, created recipes screen and implement routes

--- app/src/main/java/com/cristian/apprecetas/MainActivity.kt
+++ app/src/main/java/com/cristian/apprecetas/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cristian.apprecetas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,9 +11,21 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cristian.apprecetas.ui.Routes
+import com.cristian.apprecetas.ui.Routes.*
+import com.cristian.apprecetas.ui.recipeslist.RecipesListScreen
+import com.cristian.apprecetas.ui.recipeslist.RecipesViewModel
 import com.cristian.apprecetas.ui.theme.AppRecetasTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val recipesViewModel: RecipesViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +35,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navigationController = rememberNavController()
+                    NavHost(
+                        navController = navigationController,
+                        startDestination = Screen1.route
+                    ) {
+                        composable(Screen1.route) { RecipesListScreen(recipesViewModel) }
+                    }
+                    //RecipesListScreen(recipesViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AppRecetasTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/cristian/apprecetas/ui/recipeslist/RecipesListScreen.kt
@@ -0,0 +1,104 @@
+package com.cristian.apprecetas.ui.recipeslist
+
+import android.widget.Toast
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.cristian.apprecetas.data.network.response.ApiResponseStatus
+import com.cristian.apprecetas.domain.model.RecipesUI
+import com.cristian.apprecetas.ui.ErrorDialog
+import com.cristian.apprecetas.ui.LoadingWheel
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun RecipesListScreen(recipesViewModel: RecipesViewModel) {
+
+    val status = recipesViewModel.status.value
+    val recipes = recipesViewModel.recipesList.value.groupBy { it.strTags }
+    val context = LocalContext.current
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        recipes.forEach { (strTags, recipesUI) ->
+            stickyHeader {
+                Text(
+                    text = strTags,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.LightGray),
+                    fontSize = 16.sp
+                )
+            }
+            items(recipesUI) {
+                RecipeItems(recipesUI = it) {
+                    Toast.makeText(
+                        context,
+                        it.nameMeal,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+
+        }
+    }
+
+    if (status is ApiResponseStatus.Loading) {
+        LoadingWheel()
+    } else if (status is ApiResponseStatus.Error) {
+        ErrorDialog(status.messageId) { recipesViewModel.resetApiResponseStatus() }
+    }
+}
+
+@Composable
+fun RecipeItems(recipesUI: RecipesUI, onRecipeClicked: (RecipesUI) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onRecipeClicked(recipesUI) },
+        elevation = 15.dp,
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(2.dp, Color.White, RoundedCornerShape(8.dp)),
+                model = recipesUI.strMealThumb,
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+            )
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(text = recipesUI.nameCategory, color = Color.Gray)
+                Text(text = recipesUI.nameMeal, fontSize = 20.sp)
+                Text(text = recipesUI.nameArea, color = Color.Gray)
+            }
+        }
+    }
+}
