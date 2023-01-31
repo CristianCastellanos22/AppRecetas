703e209409559bfa5ae9cc45634502bed401aade
ccastellanostribal <ccastellanos@tribalworldwide.gt>
2023-01-30 21:30:08 -0500

    Created client and modelUI

---
+++ app/src/main/java/com/cristian/apprecetas/data/network/RecipesClient.kt
@@ -0,0 +1,10 @@
+package com.cristian.apprecetas.data.network
+
+import com.cristian.apprecetas.data.network.response.RecipesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface RecipesClient {
+    @GET("/meals")
+    suspend fun doGetRecipes(): Response<List<RecipesResponse>>
+}
